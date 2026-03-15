95f4336a684a7ab67d77c8093e07b0679029ded7
Chimaobi Chinaka <chimaobi01@gmail.com>
2026-03-15 01:13:40 +0100

    update PORT variable

--- index.js
+++ index.js
@@ -71,9 +71,10 @@ app.post('/api/notes', (request, response) => {
 
 
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server is running on ${PORT}`);
+const PORT = process.env.PORT || 3001
+app.listen(PORT, ()=>{
+    console.log(`Server is running on ${PORT}`);
+})
 
 
 function generateId() {
